2c0fd3616a57bbe883efc98d4cb37cb978a0e544
eliezer-chaves <chaves.eliezer@outlook.com>
2024-11-21 15:39:08 -0300

    updates

--- js/controllers/jurosComAportes.js
+++ js/controllers/jurosComAportes.js
@@ -36,23 +36,21 @@ function atualizarAnos() {
 }
 
 // Adiciona um ouvinte de evento para o formulário
-document.getElementById('financeForm').addEventListener('keypress', function (event) {
+document.getElementById('jurosComAportes').addEventListener('keypress', function (event) {
     // Verifica se a tecla pressionada é 'Enter'
     if (event.key === 'Enter') {
         event.preventDefault(); // Impede o comportamento padrão de enviar o formulário
-        calcular(); // Chama a função de cálculo
+        calcularJurosComAportes(); // Chama a função de cálculo
     }
 });
 
-function calcular() {
+function calcularJurosComAportes() {
     const aporteMensal = parseFloat(document.getElementById('aporteMensal').value.replace(/[R$\s.]/g, '').replace(',', '.'));
     const numPeriodos = parseInt(document.getElementById('numPeriodos').value);
     const taxaJurosAnual = parseFloat(document.getElementById('taxaJuros').value.replace(/[.\s%]/g, '').replace(',', '.')) / 100;
-    const considerarInflacao = document.getElementById('considerarInflacao').checked;
-    const inflacaoAnual = considerarInflacao ? parseFloat(document.getElementById('inflacao').value.replace(/[.\s%]/g, '').replace(',', '.')) / 100 : 0;
 
-    if (isNaN(aporteMensal) || isNaN(numPeriodos) || isNaN(taxaJurosAnual) || (considerarInflacao && isNaN(inflacaoAnual))) {
-        alert('Preencha todos os campos corretamente.');
+    if (isNaN(aporteMensal) || isNaN(numPeriodos) || isNaN(taxaJurosAnual)) {
+        $('#modalAviso').modal('show')
         return;
     }
 
@@ -81,62 +79,5 @@ function calcular() {
     const taxaDiariaPercentual = taxaJurosDiaria * 100;
     document.getElementById('taxaDiaria').value = taxaDiariaPercentual.toLocaleString('pt-BR', { minimumFractionDigits: 4, maximumFractionDigits: 4 }) + ' %';
 
-    // Ajuste pela inflação, se necessário
-    if (considerarInflacao && inflacaoAnual > 0) {
-        const inflacaoMensal = Math.pow(1 + inflacaoAnual, 1 / 12) - 1;
-        let montanteComInflacao = montanteFinal;
-
-        // Aplicar o desconto da inflação sobre o montante final
-        for (let i = 0; i < numPeriodos; i++) {
-            montanteComInflacao /= (1 + inflacaoMensal);
-        }
-
-        const totalInflacao = montanteFinal - montanteComInflacao;
-
-        // Exibir montante ajustado pela inflação e o total da inflação descontada
-        document.getElementById('montanteComInflacao').value = 'R$ ' + montanteComInflacao.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-        document.getElementById('totalInflacao').value = 'R$ ' + totalInflacao.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-    } else {
-        document.getElementById('montanteComInflacao').value = 'R$ ' + montanteFinal.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-        document.getElementById('totalInflacao').value = 'R$ 0,00';
-    }
-
     document.getElementById('resultados').style.display = 'block';
-}
-
-function toggleTheme() {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
-    body.classList.toggle('light-mode');
-
-    const btn = document.getElementById('toggleTheme');
-    const lampIcon = btn.querySelector('.lamp-icon');
-
-    if (body.classList.contains('dark-mode')) {
-        btn.classList.remove('lamp-off');
-        btn.classList.add('lamp-on');
-        lampIcon.textContent = '💡';
-    } else {
-        btn.classList.remove('lamp-on');
-        btn.classList.add('lamp-off');
-        lampIcon.textContent = '🌑';
-    }
-}
- // Função para alternar os campos de inflação
- function toggleInflacaoInputs() {
-    const considerarInflacao = document.getElementById('considerarInflacao').checked;
-    const inflacaoInputs = document.getElementById('inflacaoInputs');
-    const inflacaoResults = document.getElementById('inflacaoResults');
-    const inflacaoTotal = document.getElementById('inflacaoTotal');
-    const montanteComInflacao = document.getElementById('montanteComInflacao');
-
-    if (considerarInflacao) {
-        inflacaoInputs.style.display = 'flex';
-        inflacaoResults.style.display = 'flex';
-        inflacaoTotal.style.display = 'flex';
-    } else {
-        inflacaoInputs.style.display = 'none';
-        inflacaoResults.style.display = 'none';
-        inflacaoTotal.style.display = 'none';
-    }
 }
---
+++ js/controllers/magicNumber.js
@@ -0,0 +1,57 @@
+// Máscara de entrada para o aporte mensal
+IMask(document.getElementById('preco'), {
+    mask: 'R$ num',
+    blocks: {
+        num: {
+            mask: Number,
+            thousandsSeparator: '.',
+            radix: ','
+        }
+    }
+});
+// Máscara de entrada para o aporte mensal
+IMask(document.getElementById('dividendo'), {
+    mask: 'R$ num',
+    blocks: {
+        num: {
+            mask: Number,
+            thousandsSeparator: '.',
+            radix: ','
+        }
+    }
+});
+
+// Adiciona um ouvinte de evento para o formulário
+document.getElementById('magicNumberForm').addEventListener('keypress', function (event) {
+    // Verifica se a tecla pressionada é 'Enter'
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Impede o comportamento padrão de enviar o formulário
+        calculoMagicNumber(); // Chama a função de cálculo
+    }
+});
+
+
+function calculoMagicNumber() {
+    const preco = parseFloat(document.getElementById('preco').value.replace(/[R$\s.]/g, '').replace(',', '.'));
+    const dividendo = parseFloat(document.getElementById('dividendo').value.replace(/[R$\s.]/g, '').replace(',', '.'));
+
+    if (isNaN(preco) || isNaN(dividendo)) {
+        //alert('Preencha todos os campos corretamente.');
+        $('#modalAviso').modal('show')
+        return;
+    }
+
+    const numCotas = preco / dividendo;
+    const investimentoTotal = numCotas * preco;
+    const rendaMensal = numCotas*dividendo;
+    const dy = (dividendo/preco)*100;
+
+    document.getElementById('numCotas').innerHTML = 'Número de Cotas Necessárias: <b>' + parseInt(numCotas) + '</b> cotas.'
+    document.getElementById('investimentoTotal').innerHTML = 'Investimento Necessário: <b>R$ ' + investimentoTotal.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })+'</b>.';
+    document.getElementById('rendaMensal').innerHTML = 'Ganho mensal: <b>R$ ' + rendaMensal.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })+'</b>.';
+    document.getElementById('dy').innerHTML = 'Dividend Yield (mensal): <b>' + dy.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + ' %</b>.';
+
+
+    document.getElementById('resultado').style.display = 'block';
+
+}
